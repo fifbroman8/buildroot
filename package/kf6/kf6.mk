d6e783c8126b494669ce0588053e9359bc083caf
fifbroman8 <fifbroman8@outlook.com>
2026-08-12 09:11:48 +0800

    Update KF6 version to 6.28

--- package/kf6/kf6.mk
+++ package/kf6/kf6.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KF6_VERSION_MAJOR = 6.26
+KF6_VERSION_MAJOR = 6.28
 KF6_VERSION = $(KF6_VERSION_MAJOR).0
 KF6_SITE = https://download.kde.org/stable/frameworks/$(KF6_VERSION_MAJOR)
 
